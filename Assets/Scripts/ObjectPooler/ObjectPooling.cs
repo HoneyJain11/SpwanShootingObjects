af5d9e3fa92366fe40dc243a27b0d3cd99e6ac1d
HoneyJain11 <hrjain911@gmail.com>
2022-05-14 13:41:47 +0530

    Added Object pooling for bullet

---
+++ Assets/Scripts/ObjectPooler/ObjectPooling.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : GenericSingleton<ObjectPooling>
+{
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        public int itemRequire;
+        public GameObject objectPrefab;
+
+    }
+
+    public List<ObjectPoolItem> objectPoolItems;
+    List<GameObject> pooledRequireObjects;
+  
+    private void OnEnable()
+    {
+        pooledRequireObjects = new List<GameObject>();
+        foreach (ObjectPoolItem poolItem in objectPoolItems)
+        {
+            for (int i = 0; i < poolItem.itemRequire; i++)
+            {
+                GameObject gameObject = (GameObject)Instantiate(poolItem.objectPrefab);
+                gameObject.SetActive(false);
+                pooledRequireObjects.Add(gameObject);
+            }
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledRequireObjects.Count; i++)
+        {
+            if (!pooledRequireObjects[i].activeInHierarchy)
+            {
+                return pooledRequireObjects[i];
+            }
+
+        }
+        return null;
+    }
+}
